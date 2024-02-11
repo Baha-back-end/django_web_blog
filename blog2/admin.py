cc4789f6bceb042227dfa0b2cdb44620cb35a486
Baha-back-end <fancybax@gmail.com>
2024-02-11 23:27:17 +0500

    developed DB and admin panel

--- blog2/admin.py
+++ blog2/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title','category', 'views', 'created_at', 'updated_at')
+    list_filter = ('category', 'created_at')
+    readonly_fielsds = ('views',)
+
+
+
+admin.site.register(Category)
+admin.site.register(Article, ArticleAdmin)
